b08aacfa550a620dc0c4b4cc816ffaa8d1023f58
Junyoung Lee <99023627+BangTtagGum@users.noreply.github.com>
2023-09-15 14:25:00 +0900

    Project Settings

---
+++ src/main/java/com/example/miniprojectdelivery/MiniprojectDeliveryApplication.java
@@ -0,0 +1,13 @@
+package com.example.miniprojectdelivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniprojectDeliveryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiniprojectDeliveryApplication.class, args);
+    }
+
+}
