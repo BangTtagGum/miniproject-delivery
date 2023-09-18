6f585b470a828ce6a64b07b8ea7845f47de78db9
LeeJiHun1 <dlwlgnsaaa@naver.com>
2023-09-18 18:26:04 +0900

    User, Customer 가입 로그인 완료

---
+++ src/main/java/com/example/miniprojectdelivery/utill/exception/GlobalControllerAdvice.java
@@ -0,0 +1,24 @@
+package com.example.miniprojectdelivery.utill.exception;
+
+import com.example.miniprojectdelivery.model.Msg;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+@RestControllerAdvice
+public class GlobalControllerAdvice {
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public Msg handlerIllegalArgumentException(HttpServletResponse response, IllegalArgumentException ex) {
+        response.setStatus(400);
+        String msg = ex.getMessage();
+        return new Msg(400, msg);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public Msg handlerMethodArgumentNotValidException (HttpServletResponse response, MethodArgumentNotValidException  ex) {
+        response.setStatus(400);
+        String msg = ex.getBindingResult().getAllErrors().get(0).getDefaultMessage();
+        return new Msg(400, msg);
+    }
+}
