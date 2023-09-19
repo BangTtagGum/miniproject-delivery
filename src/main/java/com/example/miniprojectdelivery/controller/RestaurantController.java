8d5b6d6c2f838e77e4f01aad0f91ab2879a573d7
Junyoung Lee <99023627+BangTtagGum@users.noreply.github.com>
2023-09-19 20:55:25 +0900

    Feat: RestaurantController Validation 추가

--- src/main/java/com/example/miniprojectdelivery/controller/RestaurantController.java
+++ src/main/java/com/example/miniprojectdelivery/controller/RestaurantController.java
@@ -1,11 +1,16 @@
 package com.example.miniprojectdelivery.controller;
 
 import com.example.miniprojectdelivery.dto.MessageResponseDto;
-import com.example.miniprojectdelivery.dto.RestaurantRequestDto;
-import com.example.miniprojectdelivery.dto.RestaurantResponseDto;
+import com.example.miniprojectdelivery.dto.restaurant.RestaurantRequestDto;
+import com.example.miniprojectdelivery.dto.restaurant.RestaurantResponseDto;
 import com.example.miniprojectdelivery.service.RestaurantService;
+import com.example.miniprojectdelivery.utill.security.UserDetailsImpl;
+import jakarta.validation.Valid;
 import java.util.List;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,28 +22,27 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/api/restaurants")
 public class RestaurantController {
 
     private final RestaurantService restaurantService;
 
-    public RestaurantController(RestaurantService restaurantService) {
-        this.restaurantService = restaurantService;
-    }
-
     // 업장 생성
     @PostMapping
     public RestaurantResponseDto restaurantCreate(
-            @RequestBody RestaurantRequestDto restaurantRequestDto
+            @AuthenticationPrincipal UserDetailsImpl userDetails,
+            @Valid @RequestBody RestaurantRequestDto restaurantRequestDto
+
     ) {
-        return restaurantService.restaurantCreate(restaurantRequestDto);
+        return restaurantService.restaurantCreate(userDetails.getUser(),restaurantRequestDto);
     }
 
     // 업장 수정
     @PutMapping("/{restaurantId}")
     public RestaurantResponseDto restaurantUpdate(
             @PathVariable Long restaurantId,
-            @RequestBody RestaurantRequestDto restaurantRequestDto
+            @Valid @RequestBody RestaurantRequestDto restaurantRequestDto
     ) {
         return restaurantService.restaurantUpdate(restaurantId, restaurantRequestDto);
     }
@@ -61,11 +65,10 @@ public class RestaurantController {
 
     // 키워드로 업장 검색
     @GetMapping("/search")
-    public List<RestaurantResponseDto> searchRestaurant(
+    public ResponseEntity<List<RestaurantResponseDto>> searchRestaurant(Model model,
             @RequestParam(value = "keyword") String keyword
     ) {
-        return restaurantService.searchRestaurant(keyword);
+        return ResponseEntity.ok().body(restaurantService.searchRestaurant(keyword));
     }
-
 
 }
