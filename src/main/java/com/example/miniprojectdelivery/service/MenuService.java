3bb99271157bf1c45f7c3c15ca6f144e2d89cf5e
Junyoung Lee <99023627+BangTtagGum@users.noreply.github.com>
2023-09-19 21:00:37 +0900

    Feat: MenuService 전달받는 Dto 수정

--- src/main/java/com/example/miniprojectdelivery/service/MenuService.java
+++ src/main/java/com/example/miniprojectdelivery/service/MenuService.java
@@ -1,7 +1,8 @@
 package com.example.miniprojectdelivery.service;
 
-import com.example.miniprojectdelivery.dto.MemuResponseDto;
-import com.example.miniprojectdelivery.dto.MenuRequestDto;
+import com.example.miniprojectdelivery.dto.menu.MenuResponseDto;
+import com.example.miniprojectdelivery.dto.menu.MenuCreateRequestDto;
+import com.example.miniprojectdelivery.dto.menu.MenuUpdateRequestDto;
 import com.example.miniprojectdelivery.dto.MessageResponseDto;
 import com.example.miniprojectdelivery.model.Menu;
 import com.example.miniprojectdelivery.model.Restaurant;
@@ -9,8 +10,6 @@ import com.example.miniprojectdelivery.repository.MenuRepository;
 import com.example.miniprojectdelivery.repository.RestaurantRepository;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -21,32 +20,35 @@ public class MenuService {
 
     private final MenuRepository menuRepository;
     private final RestaurantRepository restaurantRepository;
-    public MemuResponseDto createMenu(MenuRequestDto requestDto) {
+
+    public MenuResponseDto createMenu(MenuCreateRequestDto requestDto) {
         Restaurant restaurant = restaurantRepository.findById(requestDto.getRestaurantId()).orElseThrow(
                 ()-> new IllegalArgumentException("음식점을 찾을 수 없습니다"));
         Menu menu = menuRepository.save(new Menu(requestDto, restaurant));
-       return new MemuResponseDto(menu);
+       return new MenuResponseDto(menu);
     }
 
-    public List<MemuResponseDto> getMenus() {
+    public List<MenuResponseDto> getMenus() {
         List<Menu> menuList = menuRepository.findAll();
-        return menuList.stream().map(MemuResponseDto::new).toList();
+        return menuList.stream().map(MenuResponseDto::new).toList();
     }
 
-    public MemuResponseDto selectMenu(Long id) {
+    public MenuResponseDto selectMenu(Long id) {
         Menu menu = findMenu(id);
-        return new MemuResponseDto(menu);
+        return new MenuResponseDto(menu);
     }
+
     @Transactional
-    public MemuResponseDto update(Long id, MenuRequestDto requestDto) {
+    public MenuResponseDto update(Long id, MenuUpdateRequestDto requestDto) {
         Menu menu = findMenu(id);
         menu.update(requestDto);
-        return new MemuResponseDto(menu);
+        return new MenuResponseDto(menu);
     }
-    public ResponseEntity<MessageResponseDto> deleteMenu(Long id) {
-        Menu menu = findMenu(id);
-        return ResponseEntity.status(HttpStatus.OK).body(new MessageResponseDto("메뉴 삭제 성공!"));
 
+    public MessageResponseDto deleteMenu(Long id) {
+        Menu menu = findMenu(id);
+        menuRepository.delete(menu);
+        return new MessageResponseDto("메뉴 삭제 성공!");
     }
     // 선택한 메뉴가 존재하는지 검사하는 메서드
     private Menu findMenu(Long id) {
