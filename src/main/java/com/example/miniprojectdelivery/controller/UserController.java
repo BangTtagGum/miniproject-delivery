5f7dd1a2902bbc1bfd65383b04f6e9139ea2b188
Junyoung Lee <99023627+BangTtagGum@users.noreply.github.com>
2023-09-19 20:56:55 +0900

    Feat: UserController 로그인, 회원가입 페이지 접근 API 추가

--- src/main/java/com/example/miniprojectdelivery/controller/UserController.java
+++ src/main/java/com/example/miniprojectdelivery/controller/UserController.java
@@ -1,33 +1,49 @@
 package com.example.miniprojectdelivery.controller;
 
 
-import com.example.miniprojectdelivery.dto.SignupRequestDto;
+import com.example.miniprojectdelivery.dto.MessageResponseDto;
+import com.example.miniprojectdelivery.dto.user.SignupRequestDto;
 import com.example.miniprojectdelivery.model.Mail;
-import com.example.miniprojectdelivery.model.Msg;
 import com.example.miniprojectdelivery.service.UserService;
 import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-@RestController
-@RequestMapping("/api")
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/api/users")
 public class UserController {
+
     private final UserService userService;
-    public UserController(UserService userService) {
-        this.userService = userService;
-    }
-    // ResponseEntity<Map>
-    @PostMapping("/user/signup")
-    public ResponseEntity<Msg> signup(@Valid @RequestBody SignupRequestDto requestDto){
-        return userService.signup(requestDto);
+
+
+    @GetMapping("/signup-page")
+    public String signupPage() {
+        return "signup";
     }
 
-    @DeleteMapping("/user/{id}")
-    public ResponseEntity<Msg> deleteById(@PathVariable Long id){
+    @GetMapping("/login-page")
+    public String loginPage() {
+        return "login";
+    }
+
+    @PostMapping("/signup")
+    @ResponseBody
+    public ResponseEntity<MessageResponseDto> signup(@Valid @RequestBody SignupRequestDto signupRequestDto){
+        return userService.signup(signupRequestDto);
+//        return "redirect:/api/users/login-page";
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseBody
+    public ResponseEntity<MessageResponseDto> deleteById(@PathVariable Long id){
         return userService.deleteById(id);
     }
 
-    @PostMapping("/user/mailing")
+    @PostMapping("/mailing")
+    @ResponseBody
     public void mailing(@RequestBody Mail email){
         userService.Mailing(email);
     }
