62bfcbf119c4d27c738d98e2181ab857d985b048
Junyoung Lee <99023627+BangTtagGum@users.noreply.github.com>
2023-09-19 20:57:23 +0900

    Feat: Menu관련 Dto 수정

--- src/main/java/com/example/miniprojectdelivery/dto/menu/MenuUpdateRequestDto.java
+++ src/main/java/com/example/miniprojectdelivery/dto/menu/MenuUpdateRequestDto.java
@@ -6,10 +6,7 @@ import jakarta.validation.constraints.Size;
 import lombok.Getter;
 
 @Getter
-public class MenuCreateRequestDto {
-
-    @NotNull(message = "음식점 ID는 필수 입니다.")
-    private Long restaurantId;
+public class MenuUpdateRequestDto {
 
     private String image; // S3연동예정, 더미필드(임시)
 
--- src/main/java/com/example/miniprojectdelivery/dto/menu/MenuViewResponseDto.java
+++ src/main/java/com/example/miniprojectdelivery/dto/menu/MenuViewResponseDto.java
@@ -1,22 +1,21 @@
 package com.example.miniprojectdelivery.dto.menu;
 
+
 import com.example.miniprojectdelivery.model.Menu;
 import lombok.Getter;
 
-
 @Getter
-public class MenuResponseDto {
-    public Long id;
-    private Long restaurantId;
-    private String image; // S3연동예정, 더미필드(임시)
+public class MenuViewResponseDto {
+
+    private String image;
     private String name;
     private int cost;
 
-    public MenuResponseDto(Menu menu) {
-        this.id = menu.getId();
-        this.restaurantId = menu.getRestaurant().getId();
+    public MenuViewResponseDto(Menu menu) {
         this.image = menu.getImage();
         this.name = menu.getName();
         this.cost = menu.getCost();
     }
 }
+
+
