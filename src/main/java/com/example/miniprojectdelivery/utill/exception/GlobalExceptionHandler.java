c98f54169290da14d5aa54519c076fbcf18dec3a
Junyoung Lee <99023627+BangTtagGum@users.noreply.github.com>
2023-09-19 20:12:52 +0900

    Refactor: GlobalExceptionHandler 이름 변경

--- src/main/java/com/example/miniprojectdelivery/utill/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/miniprojectdelivery/utill/exception/GlobalExceptionHandler.java
@@ -1,32 +1,40 @@
 package com.example.miniprojectdelivery.utill.exception;
 
-import com.example.miniprojectdelivery.model.Msg;
+import com.example.miniprojectdelivery.dto.MessageResponseDto;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+@Slf4j
 @RestControllerAdvice
-public class GlobalControllerAdvice {
+public class GlobalExceptionHandler {
 
     @ExceptionHandler(IllegalArgumentException.class)
-    public Msg handlerIllegalArgumentException(HttpServletResponse response,
+    public MessageResponseDto handlerIllegalArgumentException(HttpServletResponse response,
             IllegalArgumentException ex) {
+        log.error(ex.getMessage());
         response.setStatus(400);
         String msg = ex.getMessage();
-        return new Msg(400, msg);
+        return new MessageResponseDto(msg);
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public Msg handlerMethodArgumentNotValidException(HttpServletResponse response,
+    public MessageResponseDto handlerMethodArgumentNotValidException(HttpServletResponse response,
             MethodArgumentNotValidException ex) {
+        log.error(ex.getMessage());
         response.setStatus(400);
         String msg = ex.getBindingResult().getAllErrors().get(0).getDefaultMessage();
-        return new Msg(400, msg);
+        return new MessageResponseDto(msg);
     }
 
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(Exception.class)
-    public String ExHandler(Exception e) {
-        return e.getMessage();
+    public MessageResponseDto ExHandler(Exception ex) {
+        log.error(ex.getMessage());
+        return new MessageResponseDto(ex.getMessage());
     }
 }
