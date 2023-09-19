d09f551c194777a824bff00e0c8a10a42a285bd1
Junyoung Lee <99023627+BangTtagGum@users.noreply.github.com>
2023-09-19 10:18:09 +0900

    Feat: 리뷰 생성시 매개변수 인자로 User 추가

--- src/main/java/com/example/miniprojectdelivery/controller/ReviewController.java
+++ src/main/java/com/example/miniprojectdelivery/controller/ReviewController.java
@@ -1,12 +1,15 @@
 package com.example.miniprojectdelivery.controller;
 
 import com.example.miniprojectdelivery.dto.BaseResponse;
-import com.example.miniprojectdelivery.dto.ReviewRequestDto;
+import com.example.miniprojectdelivery.dto.ReviewCreateRequestDto;
 import com.example.miniprojectdelivery.dto.ReviewResponseDto;
+import com.example.miniprojectdelivery.dto.ReviewUpdateRequestDto;
 import com.example.miniprojectdelivery.service.ReviewService;
-import javax.validation.Valid;
+import com.example.miniprojectdelivery.utill.security.UserDetailsImpl;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,26 +20,26 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/restaurants/")
+@RequestMapping("/api/reviews")
 public class ReviewController {
 
     private final ReviewService reviewService;
 
-    @PostMapping("/{restaurantId}/reviews")
-    public ReviewResponseDto createReview(@PathVariable Long restaurantId,
-            @RequestBody @Valid ReviewRequestDto requestDto) {
-        ReviewResponseDto responseDto = reviewService.createReview(restaurantId, requestDto);
+    @PostMapping
+    public ReviewResponseDto createReview(@AuthenticationPrincipal UserDetailsImpl userDetails,
+            @RequestBody @Valid ReviewCreateRequestDto requestDto) {
+        ReviewResponseDto responseDto = reviewService.createReview(userDetails.getUser(), requestDto);
         return responseDto;
     }
 
-    @PutMapping("/reviews/{id}")
+    @PutMapping("/{id}")
     public ReviewResponseDto updateReview(@PathVariable Long id,
-            @RequestBody @Valid ReviewRequestDto requestDto) {
+            @RequestBody @Valid ReviewUpdateRequestDto requestDto) {
         ReviewResponseDto responseDto = reviewService.updateReview(id, requestDto);
         return responseDto;
     }
 
-    @DeleteMapping("/reviews/{id}")
+    @DeleteMapping("/{id}")
     public BaseResponse deleteReview(@PathVariable Long id) {
         String message = reviewService.deleteReview(id);
         return new BaseResponse(HttpStatus.OK, message);
