91b41e01fbc83bccff369cccb3fbb76ce7f741e3
JH <jihyeon4956@gmail.com>
2023-09-21 17:00:46 +0900

    Feat: controller>TestController 생성(HTML 확인용)

---
+++ src/main/java/com/example/miniprojectdelivery/controller/TestController.java
@@ -0,0 +1,37 @@
+package com.example.miniprojectdelivery.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/test")
+public class TestController {
+
+    // 반환사이트 확인용 임시 Controller
+
+    @GetMapping("/review")
+    public String review() {
+        return "review";
+    }
+
+    @GetMapping("/search")
+    public String search() {
+        return "search";
+    }
+
+    @GetMapping("/store")
+    public String store() {
+        return "store";
+    }
+    @GetMapping("/customer")
+    public String team() {
+        return "customer";
+    }
+    @GetMapping("/owner")
+    public String service() {
+        return "owner";
+    }
+
+
+}
