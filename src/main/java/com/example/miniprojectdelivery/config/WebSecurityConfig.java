044588c9581cca019f67c06a214e993a4835a820
JH <jihyeon4956@gmail.com>
2023-09-21 17:30:17 +0900

    Feat: login.html 생성 - 로그인은 되지만 토큰 저장 null 오류 확인중

--- src/main/java/com/example/miniprojectdelivery/config/WebSecurityConfig.java
+++ src/main/java/com/example/miniprojectdelivery/config/WebSecurityConfig.java
@@ -58,8 +58,8 @@ public class WebSecurityConfig {
                         // test용
                         .requestMatchers("/test/**").permitAll()  // html 모두 허용
 
+                        .requestMatchers("/css/**", "/js/**", "/img/**", "/lib/**", "/scss/**").permitAll()
                         .requestMatchers("/").permitAll()
-                        .requestMatchers("/css/**", "/js/**", "/images/**", "/lib/**", "/scss/**").permitAll() // resources 접근 허용 설정
                         .requestMatchers("/api/users/**").permitAll() // '/api/user/'로 시작하는 요청 모두 접근 허가
 
                         .anyRequest().authenticated() // 그 외 모든 요청 인증처리
