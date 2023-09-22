a021579add1618207a0bb784abebbf28a01a9b20
JH <jihyeon4956@gmail.com>
2023-09-22 15:56:07 +0900

    Refactor: SignUpRequestDto @NoArgsConstructor 추가

--- src/main/java/com/example/miniprojectdelivery/dto/user/SignupRequestDto.java
+++ src/main/java/com/example/miniprojectdelivery/dto/user/SignupRequestDto.java
@@ -4,11 +4,13 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor
 public class SignupRequestDto {
 
     @Pattern(
