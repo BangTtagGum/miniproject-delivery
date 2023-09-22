60ea1c72000c4cda0d965194bf2140a649531b4b
Junyoung Lee <99023627+BangTtagGum@users.noreply.github.com>
2023-09-22 20:28:37 +0900

    Fix: Mail인증정보 저장 로직 수정
    MYSQL -> Redis

--- src/main/java/com/example/miniprojectdelivery/controller/UserController.java
+++ src/main/java/com/example/miniprojectdelivery/controller/UserController.java
@@ -3,7 +3,7 @@ package com.example.miniprojectdelivery.controller;
 
 import com.example.miniprojectdelivery.dto.MessageResponseDto;
 import com.example.miniprojectdelivery.dto.user.SignupRequestDto;
-import com.example.miniprojectdelivery.model.Mail;
+import com.example.miniprojectdelivery.dto.user.MailRequestDto;
 import com.example.miniprojectdelivery.service.UserService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -51,7 +51,7 @@ public class UserController {
 
     @PostMapping("/mailing")
     @ResponseBody
-    public void mailing(@RequestBody Mail email){
+    public void mailing(@RequestBody @Valid MailRequestDto email){
         userService.Mailing(email);
     }
 
