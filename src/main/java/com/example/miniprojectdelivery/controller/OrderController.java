aa9f1df9b70c4c869cb40409e8e2ad17c2b7a8bb
Junyoung Lee <99023627+BangTtagGum@users.noreply.github.com>
2023-09-23 18:09:49 +0900

    Feat: 주문 사용자 인증 Validation 추가
    고객만 주문 가능,
    자신의 음식점의 주문만 조회 가능,
    자신의 음식점의 주문만 배달 가능

--- src/main/java/com/example/miniprojectdelivery/controller/OrderController.java
+++ src/main/java/com/example/miniprojectdelivery/controller/OrderController.java
@@ -42,45 +42,49 @@ public class OrderController {
     }
 
     /**
-     * 사장님의 자신의 음식점 고객들 주문 조회
+     * 사장님의 자신의 음식점 고객들 주문 조회 메소드
      *
-     * 해당 가게 사장님만 주문 조회 가능
-     *
+     * @param restaurantId 음식점 ID
      * @return
      */
     @Secured("ROLE_OWNER")
     @GetMapping("/restaurants/{restaurantId}")
-    public List<OrderResponseDto> getOrdersByRestaurantId(@PathVariable Long restaurantId) {
-        return orderService.getOrdersByRestaurantId(restaurantId);
+    public List<OrderResponseDto> getOrdersByRestaurantId(
+            @AuthenticationPrincipal UserDetailsImpl userDetails, @PathVariable Long restaurantId) {
+        User user = userDetails.getUser();
+        return orderService.getOrdersByRestaurantId(user, restaurantId);
     }
 
     /**
      * 고객의 주문 생성 메소드
      *
      * 고객만 주문 생성 가능
+     *
      * @param userDetails 주문하는 고객의 정보
-     * @param requestDto 주문 내역
+     * @param requestDto  주문 내역
      */
-    @Secured("ROLE_CUSOTMER")
+    @Secured("ROLE_CUSTOMER")
     @PostMapping
     public OrderResponseDto createOrder(@AuthenticationPrincipal UserDetailsImpl userDetails,
-                                        @RequestBody
-                                        OrderCreateRequestDto requestDto) {
-        Long userId = userDetails.getUser().getId();
-        return orderService.createOrder(userId, requestDto);
+            @RequestBody
+            OrderCreateRequestDto requestDto) {
+        User user = userDetails.getUser();
+        return orderService.createOrder(user, requestDto);
     }
 
     /**
      * 사장님의 주문 배달 완료 처리
-     *
+     * <p>
      * 자신의 가게만 주문 배달 완료 처리 가능
+     *
      * @param orderId 배달 완료 처리할 주문 ID
      * @return
      */
     @Secured("ROLE_OWNER")
     @PostMapping("{orderId}/deliver")
-    public MessageResponseDto deliveryOrder(@PathVariable Long orderId) {
-        orderService.deliveryOrder(orderId);
+    public MessageResponseDto deliveryOrder(@AuthenticationPrincipal UserDetailsImpl userDetails, @PathVariable Long orderId) {
+        User owner = userDetails.getUser();
+        orderService.deliveryOrder(owner,orderId);
         return new MessageResponseDto("배달 완료");
     }
 
