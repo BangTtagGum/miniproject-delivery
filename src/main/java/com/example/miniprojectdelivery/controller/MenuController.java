579840456f3257f7226c591c8157aa54859f848f
Junyoung Lee <99023627+BangTtagGum@users.noreply.github.com>
2023-09-23 18:07:45 +0900

    Feat: 메뉴 API 사용자 인증 Validation 추가

--- src/main/java/com/example/miniprojectdelivery/controller/MenuController.java
+++ src/main/java/com/example/miniprojectdelivery/controller/MenuController.java
@@ -4,11 +4,14 @@ import com.example.miniprojectdelivery.dto.menu.MenuCreateRequestDto;
 import com.example.miniprojectdelivery.dto.menu.MenuResponseDto;
 import com.example.miniprojectdelivery.dto.menu.MenuUpdateRequestDto;
 import com.example.miniprojectdelivery.dto.MessageResponseDto;
+import com.example.miniprojectdelivery.model.User;
 import com.example.miniprojectdelivery.service.MenuService;
+import com.example.miniprojectdelivery.utill.security.UserDetailsImpl;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -25,19 +28,21 @@ public class MenuController {
     /**
      * 메뉴 생성 메소드 - 자신의 음식점에만 메뉴 생성 가능
      *
-     * @param image 생성하려는 메뉴의 이미지
+     * @param image        생성하려는 메뉴의 이미지
      * @param restaurantId 생성하려는 음식점의 id
-     * @param name 생성하려는 메뉴의 이름
-     * @param cost 생성하려는 메뉴의 가격
+     * @param name         생성하려는 메뉴의 이름
+     * @param cost         생성하려는 메뉴의 가격
      */
     @Secured("ROLE_OWNER")
     @PostMapping
     public MenuResponseDto createMenu(
+            @AuthenticationPrincipal UserDetailsImpl userDetails,
             @Valid @RequestParam("image") MultipartFile image,
             @Valid @RequestParam("restaurantId") Long restaurantId,
             @Valid @RequestParam("name") String name,
             @Valid @RequestParam("cost") int cost) throws IOException {
-        return menuService.createMenu(image, restaurantId, name, cost);
+        User user = userDetails.getUser();
+        return menuService.createMenu(user,image, restaurantId, name, cost);
     }
 
     @GetMapping
@@ -61,11 +66,13 @@ public class MenuController {
     @Secured("ROLE_OWNER")
     @PutMapping("/{id}")
     public MenuResponseDto updateMenu(
+            @AuthenticationPrincipal UserDetailsImpl userDetails,
             @PathVariable Long id,
             @RequestParam(value = "image") MultipartFile newImage,
             @RequestParam("name") String name,
             @RequestParam("cost") int cost) throws IOException {
-        return menuService.update(id, newImage, name, cost);
+        User user = userDetails.getUser();
+        return menuService.update(user, id, newImage, name, cost);
     }
 
     /**
@@ -73,8 +80,11 @@ public class MenuController {
      *
      * @param id 삭제하려는 메뉴 ID
      */
+    @Secured("ROLE_OWNER")
     @DeleteMapping("/{id}")
-    public ResponseEntity<MessageResponseDto> deleteMenu(@PathVariable Long id){
-        return ResponseEntity.ok().body(menuService.deleteMenu(id));
+    public ResponseEntity<MessageResponseDto> deleteMenu(
+            @AuthenticationPrincipal UserDetailsImpl userDetails, @PathVariable Long id) {
+        User user = userDetails.getUser();
+        return ResponseEntity.ok().body(menuService.deleteMenu(user, id));
     }
 }
